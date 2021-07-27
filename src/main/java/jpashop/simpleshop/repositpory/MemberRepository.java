b3fbd8607ad7148aa63cab1e777a46e4aa17da9b
ohky <oqoq59@gmail.com>
2021-07-27 23:56:23 +0900

    MemberRepository - add findAll methods

--- src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
+++ src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
@@ -18,5 +19,10 @@ public class MemberRepository {
 
     public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 }
