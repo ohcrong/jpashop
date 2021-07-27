0cf5d6185089357d8da2b2c83ccd2a20e5a7c5b4
ohky <oqoq59@gmail.com>
2021-07-28 00:07:33 +0900

    MemberRepository - add findByName method

--- src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
+++ src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
@@ -7,14 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
+@Repository //등록하면 @SpringBootApplication의 하위 페케지 component는 모두 스캔 대상
 public class MemberRepository {
 
     @PersistenceContext
     private EntityManager em;
 
     public void save(Member member) {
-        em.persist(member);
+        em.persist(member);//transaction시점에 DB에 inset 쿼리
     }
 
     public Member findOne(Long id) {
@@ -23,6 +23,12 @@ public class MemberRepository {
 
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
 }
