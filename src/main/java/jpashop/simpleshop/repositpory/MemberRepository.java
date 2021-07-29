ce828923cc72d1375cf0372a38e98c75f6c055ac
ohky <oqoq59@gmail.com>
2021-07-29 23:43:51 +0900

    @RequiredArgsConstructor 추가

--- src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
+++ src/main/java/jpashop/simpleshop/repositpory/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpashop.simpleshop.repositpory;
 
 import jpashop.simpleshop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,12 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository //등록하면 @SpringBootApplication의 하위 페케지 component는 모두 스캔 대상
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+    //spring data jpa가 @PersistenceContext -> @Autowired로 사용가능
+    //final 붙이고 @RequiredArgsConstructor로 대체
 
     public void save(Member member) {
         em.persist(member);//transaction시점에 DB에 inset 쿼리
