628d7ff1e2cabdb2b1f4c68261b48642601f1736
ohky <oqoq59@gmail.com>
2021-08-15 00:39:12 +0900

    아이탬 controller 추가

---
+++ src/main/java/jpashop/simpleshop/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpashop.simpleshop.controller;
+
+import jpashop.simpleshop.domain.item.Book;
+import jpashop.simpleshop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createItem(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createBookForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+}
