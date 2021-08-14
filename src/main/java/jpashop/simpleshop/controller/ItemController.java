2d57fa8e5f1aa37087e0a1df57ba6ceb15235887
ohky <oqoq59@gmail.com>
2021-08-15 01:24:57 +0900

    아이탬 setter 사용 -> 생성 메서드 변경

--- src/main/java/jpashop/simpleshop/controller/ItemController.java
+++ src/main/java/jpashop/simpleshop/controller/ItemController.java
@@ -1,8 +1,10 @@
 package jpashop.simpleshop.controller;
 
 import jpashop.simpleshop.domain.item.Book;
+import jpashop.simpleshop.domain.item.Item;
 import jpashop.simpleshop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
 
     private final ItemService itemService;
@@ -22,10 +25,10 @@ public class ItemController {
 
     @PostMapping("/items/new")
     public String create(BookForm form) {
-        Book book = new Book();
-        book.createBook(form.getName(), form.getPrice(), form.getStockQuantity(), form.getAuthor(), form.getIsbn());
+
+        Book book = Book.createBook(form.getName(), form.getPrice(), form.getStockQuantity(), form.getAuthor(), form.getIsbn());
 
         itemService.saveItem(book);
-        return "redirect:/items";
+        return "redirect:/";
     }
 }
--- src/main/java/jpashop/simpleshop/domain/item/Book.java
+++ src/main/java/jpashop/simpleshop/domain/item/Book.java
@@ -2,6 +2,7 @@ package jpashop.simpleshop.domain.item;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
