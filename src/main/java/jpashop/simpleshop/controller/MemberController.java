6418c549151e7b7b078327a4bc1726c6a9948b7d
ohky <oqoq59@gmail.com>
2021-08-14 23:20:15 +0900

    맴버등록 포스트메핑

--- src/main/java/jpashop/simpleshop/controller/MemberController.java
+++ src/main/java/jpashop/simpleshop/controller/MemberController.java
@@ -1,10 +1,13 @@
 package jpashop.simpleshop.controller;
 
+import jpashop.simpleshop.domain.Address;
+import jpashop.simpleshop.domain.Member;
 import jpashop.simpleshop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,4 +21,17 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
