86c5dbe7e82be98b51be56bf6804d1bed129b884
kyuyoungoh <oqoq59@gmail.com>
2021-08-22 22:01:41 +0900

    add api saveMemberApi

--- src/main/java/jpashop/simpleshop/api/MemberApiController.java
+++ src/main/java/jpashop/simpleshop/api/MemberApiController.java
@@ -1,4 +1,34 @@
 package jpashop.simpleshop.api;
 
+import jpashop.simpleshop.domain.Member;
+import jpashop.simpleshop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 }
