91637bc15e2fe7dbfb3d6bafabbad421f3f57ef7
kyuyoungoh <oqoq59@gmail.com>
2021-08-24 23:52:57 +0900

    add update method for api

--- src/main/java/jpashop/simpleshop/api/MemberApiController.java
+++ src/main/java/jpashop/simpleshop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpashop.simpleshop.api;
 
 import jpashop.simpleshop.domain.Member;
 import jpashop.simpleshop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -33,6 +32,15 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member member = memberService.findOne(id);
+
+        return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
@@ -46,5 +54,18 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+
     }
 }
