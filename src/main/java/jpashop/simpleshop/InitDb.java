371b7804ff36963d0c5d541306e243376e5a256c
kyuyoungoh <oqoq59@gmail.com>
2021-08-27 23:39:48 +0900

    add initiative db data

--- src/main/java/jpashop/simpleshop/InitDb.java
+++ src/main/java/jpashop/simpleshop/InitDb.java
@@ -2,8 +2,19 @@ package jpashop.simpleshop;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @Component
 @RequiredArgsConstructor
 public class InitDb {
+
+    @Service
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+    }
 }
