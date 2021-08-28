3f6a5b8aca0b81ec102302e608a1ec9c91a36778
kyuyoungoh <oqoq59@gmail.com>
2021-08-28 17:19:03 +0900

    add initiative db data

--- src/main/java/jpashop/simpleshop/InitDb.java
+++ src/main/java/jpashop/simpleshop/InitDb.java
@@ -1,7 +1,7 @@
 package jpashop.simpleshop;
 
-import jpashop.simpleshop.domain.Address;
-import jpashop.simpleshop.domain.Member;
+import jpashop.simpleshop.domain.*;
+import jpashop.simpleshop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -26,12 +26,35 @@ public class InitDb {
     @Transactional
     @RequiredArgsConstructor
     static class InitService {
+
         private final EntityManager em;
         public void dbInit1(){
             Member member = new Member();
             member.setName("A");
             member.setAddress(new Address("seoul","111", "11100"));
             em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("JPA1");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
+
         }
 
     }
