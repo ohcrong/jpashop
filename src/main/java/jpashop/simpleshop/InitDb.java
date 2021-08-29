0ee63e8c9ffe77a20585b900a8cbe09ecd33b934
kyuyoungoh <oqoq59@gmail.com>
2021-08-29 23:08:52 +0900

    add initiative db data

--- src/main/java/jpashop/simpleshop/InitDb.java
+++ src/main/java/jpashop/simpleshop/InitDb.java
@@ -18,7 +18,9 @@ public class InitDb {
 
     @PostConstruct
     public void init() {
+
         initService.dbInit1();
+        initService.dbInit2();
     }
     //loading 시점에 생성
 
@@ -30,7 +32,7 @@ public class InitDb {
         private final EntityManager em;
         public void dbInit1(){
             Member member = new Member();
-            member.setName("A");
+            member.setName("userB");
             member.setAddress(new Address("seoul","111", "11100"));
             em.persist(member);
 
@@ -56,6 +58,33 @@ public class InitDb {
 
 
         }
+        public void dbInit2(){
+            Member member = new Member();
+            member.setName("userB");
+            member.setAddress(new Address("seoul","222", "22200"));
+            em.persist(member);
 
+            Book book1 = new Book();
+            book1.setName("SPRING1");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("SPRING2");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
+
+        }
     }
 }
