05b18d4225a71909e21473702a416b9efe84cfa7
kyuyoungoh <oqoq59@gmail.com>
2021-08-30 17:07:26 +0900

    add sample order api controller

---
+++ src/main/java/jpashop/simpleshop/api/OrderSimpleApiController.java
@@ -0,0 +1,15 @@
+package jpashop.simpleshop.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * ORDER
+ * ORDER -> MEMBER
+ * ORder -> DELIVERY
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+}
