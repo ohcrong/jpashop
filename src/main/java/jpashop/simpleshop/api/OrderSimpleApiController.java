e280ed030b43c692e738507576f78ab5a0f54c98
kyuyoungoh <oqoq59@gmail.com>
2021-09-01 18:01:10 +0900

    주문 조회 api v2

--- src/main/java/jpashop/simpleshop/api/OrderSimpleApiController.java
+++ src/main/java/jpashop/simpleshop/api/OrderSimpleApiController.java
@@ -1,7 +1,18 @@
 package jpashop.simpleshop.api;
 
+import jpashop.simpleshop.domain.Address;
+import jpashop.simpleshop.domain.Order;
+import jpashop.simpleshop.domain.OrderStatus;
+import jpashop.simpleshop.repositpory.OrderRepository;
+import jpashop.simpleshop.repositpory.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -13,4 +24,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream().
+                map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
 }
