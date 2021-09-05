ae701e92b82d1839b48e9a052f4bb397772529f1
kyuyoungoh <oqoq59@gmail.com>
2021-09-05 09:47:21 +0900

    조회 Api용 DTO 추가

--- src/main/java/jpashop/simpleshop/repositpory/OrderSimpleQueryDto.java
+++ src/main/java/jpashop/simpleshop/repositpory/OrderSimpleQueryDto.java
@@ -8,18 +8,18 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-public class SimpleOrderQueryDto {
+public class OrderSimpleQueryDto {
     private Long orderId;
     private String name;
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
 
-    public SimpleOrderQueryDto(Order order) {
-        this.orderId = order.getId();
-        this.name = order.getMember().getName();
-        this.orderDate = order.getOrderDate();
-        this.orderStatus = order.getStatus();
-        this.address = order.getDelivery().getAddress();
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
     }
 }
