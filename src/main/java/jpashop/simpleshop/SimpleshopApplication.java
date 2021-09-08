75190ad123822eb1254d4534eef5d66c27d52932
kyuyoungoh <oqoq59@gmail.com>
2021-09-08 15:41:41 +0900

    hibernate5module 임시 추가

--- src/main/java/jpashop/simpleshop/SimpleshopApplication.java
+++ src/main/java/jpashop/simpleshop/SimpleshopApplication.java
@@ -1,13 +1,22 @@
 package jpashop.simpleshop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleshopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		//hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 
 }
