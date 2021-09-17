2d02681f4c24ef4c196258e8b87c1243e15e3ad8
kyuyoungoh <oqoq59@gmail.com>
2021-09-17 18:02:04 +0900

    api용 dto 조회

---
+++ src/main/java/jpashop/simpleshop/repositpory/order/query/OrderQueryDto.java
@@ -0,0 +1,28 @@
+package jpashop.simpleshop.repositpory.order.query;
+
+import jpashop.simpleshop.domain.Address;
+import jpashop.simpleshop.domain.Delivery;
+import jpashop.simpleshop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
