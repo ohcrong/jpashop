79a08cb00b5af17595eb2b254640b7a05a78e96d
kyuyoungoh <oqoq59@gmail.com>
2021-09-18 13:48:04 +0900

    api용 dto 조회

--- src/main/java/jpashop/simpleshop/api/OrderApiController.java
+++ src/main/java/jpashop/simpleshop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpashop.simpleshop.domain.OrderItem;
 import jpashop.simpleshop.domain.OrderStatus;
 import jpashop.simpleshop.repositpory.OrderRepository;
 import jpashop.simpleshop.repositpory.OrderSearch;
+import jpashop.simpleshop.repositpory.order.query.OrderQueryDto;
+import jpashop.simpleshop.repositpory.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -61,6 +64,11 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
     @Getter
