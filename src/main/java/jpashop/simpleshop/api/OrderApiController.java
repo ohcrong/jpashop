72917a85ae6aad4967156afd511919115cc717c7
kyuyoungoh <oqoq59@gmail.com>
2021-09-19 00:20:35 +0900

    api용 dto 조회 map 추가

--- src/main/java/jpashop/simpleshop/api/OrderApiController.java
+++ src/main/java/jpashop/simpleshop/api/OrderApiController.java
@@ -71,6 +71,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_Optimization();
+    }
+
     @Getter
     static class OrderDto {
 
