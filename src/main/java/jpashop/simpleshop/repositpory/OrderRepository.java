0c58a70dd3bc6189993971a8d35de9b7f82077b3
kyuyoungoh <oqoq59@gmail.com>
2021-09-27 21:38:41 +0900

    refactor findAll with queryDsl

--- src/main/java/jpashop/simpleshop/repositpory/OrderRepository.java
+++ src/main/java/jpashop/simpleshop/repositpory/OrderRepository.java
@@ -1,6 +1,11 @@
 package jpashop.simpleshop.repositpory;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpashop.simpleshop.domain.Order;
+import jpashop.simpleshop.domain.OrderStatus;
+import jpashop.simpleshop.domain.QMember;
+import jpashop.simpleshop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -23,13 +28,32 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o join o.member m" +
-                "where o.status = :status" +
-                "and m.name = :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) //최대 1000건
-                .getResultList();
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
